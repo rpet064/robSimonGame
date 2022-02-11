4b7f218492e0b69aaaf9545d3270ceae07cd7b97
rpet064 <93119631+rpet064@users.noreply.github.com>
2022-02-11 17:16:21 +1300

    Upload Final Commit

---
+++ game.js
@@ -0,0 +1,93 @@
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+var userClickedPattern = [];
+
+var started = false;
+var level = 0;
+
+// resets the variables for the next game
+function startover(){
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
+
+// Starts the game on keypress
+$(document).keypress(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+// stores the player's button clicks in userClickedPattern
+$(".btn").click(function() {
+
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length-1);
+});
+
+// checks if player answer is correct
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+      console.log("success");
+
+      if (userClickedPattern.length === gamePattern.length){
+
+
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+
+      }
+
+    } else {
+      // End of the game sequence
+      var audio = new Audio("sounds/wrong.mp3");
+      audio.play();
+      $("h1").text("Game Over, Press Any Key to Restart");
+      $("body").addClass("game-over");
+      setTimeout(function () {
+      $('body').removeClass("game-over")}, 500);
+      startover()
+
+    }
+
+}
+
+function nextSequence() {
+// triggers every new level
+  userClickedPattern = [];
+
+  level++;
+  $("#level-title").text("Level " + level);
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
